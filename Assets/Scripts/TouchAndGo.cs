51b75794bb4abd7fea959aeed725e8e7a32c7eb1
Tony Xiao <tonyxtl@umich.edu>
2023-09-22 19:37:32 -0400

    not on fish

---
+++ Assets/Scripts/TouchAndGo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HapticFeedback : MonoBehaviour
+{
+    public XRBaseController controller;
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
